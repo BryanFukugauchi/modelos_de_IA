a3f989808d64a1bd47c46d2cd37f19d946d796f4
cc24120 <bry4n2008@gmail.com>
2026-08-21 14:15:34 -0300

    correto agora

--- DermFoundationModel/dermFoundation.py
+++ DermFoundationModel/dermFoundation.py
@@ -1,38 +1,20 @@
-import argparse
-import tensorflow as tf
-import numpy as np
+from model import build_model
+from train import train
+from infer import predict, CLASSES_HAM10000
 
-CLASSES_HAM10000 = {
-    0: "akiec - Queratose Actínica",
-    1: "bcc - Carcinoma Basocelular",
-    2: "bkl - Lesão Benigna do Tipo Queratose",
-    3: "df - Dermatofibroma",
-    4: "mel - Melanoma",
-    5: "nv - Nevo Melanocítico (Pinta comum)",
-    6: "vasc - Lesão Vascular"
-}
+class DermFoundation:
+    def __init__(self):
+        self.classes = CLASSES_HAM10000
 
-def predict(image_path, model):
-    img = tf.keras.utils.load_img(image_path, target_size=(224, 224))
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, axis=0)
+    def train(self, epochs=5, batch_size=16, save_path="derm_foundation_model.keras"):
+        train(
+            epochs=epochs,
+            batch_size=batch_size,
+            save_path=save_path
+        )
 
-    predictions = model.predict(img_array, verbose=0)
-    predicted_class = np.argmax(predictions, axis=1)[0]
-    confidence = predictions[0][predicted_class]
-
-    return predicted_class, confidence
-
-def main(model_path="derm_foundation_model.keras", image_path=None):
-    model = tf.keras.models.load_model(model_path, compile=False)
-    if image_path:
+    def predict(self, image_path, model_path="derm_foundation_model.keras"):
+        import tensorflow as tf
+        model = tf.keras.models.load_model(model_path, compile=False)
         pred_class, confidence = predict(image_path, model)
-        print(f"Diagnóstico: {CLASSES_HAM10000.get(pred_class, 'Desconhecido')} | Confiança: {confidence:.2%}")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, default="derm_foundation_model.keras")
-    parser.add_argument("--image_path", type=str, default=None)
-    args = parser.parse_args()
-
-    main(model_path=args.model_path, image_path=args.image_path)
+        return self.classes.get(pred_class, "Desconhecido"), confidence
